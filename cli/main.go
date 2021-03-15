70d306d0d259633568d4691c7f8204fa9360fdf6
viktorbarzin <vbarzin@gmail.com>
2021-03-15 23:32:56 +0000

    add cli client for repo

---
+++ cli/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
+)
+
+const (
+	useCaseFlagName  = "use-case"
+	repoRootFlagName = "repo-root"
+)
+
+var (
+	validUseCases = []string{"setup-vpn"}
+)
+
+func main() {
+	err := run()
+	if err != nil {
+		glog.Fatalf("run failed: %s", err.Error())
+	}
+}
+
+func run() error {
+	useCase := flag.String(useCaseFlagName, "", fmt.Sprintf("Use case to run. Available use cases are: %+v", validUseCases))
+	// repoRootParam := flag.String(repoRootFlagName, "", fmt.Sprintf("Path to the root of the infra repository."))
+
+	// VPN flags
+	vpnClientName := flag.String(vpnClientNameFlagName, "", fmt.Sprintf("Friendly VPN user name."))
+	vpnClientPubKey := flag.String(vpnClientPubKeyFlagName, "", fmt.Sprintf("VPN client public key."))
+
+	flag.Set("logtostderr", "true")
+	flag.Set("stderrthreshold", "WARNING")
+	flag.Set("v", "2")
+	flag.Parse()
+
+	// if *repoRootParam == "" {
+	// 	return fmt.Errorf("'-%s' flag must not be empty", repoRootFlagName)
+	// }
+	if *useCase == "" {
+		return fmt.Errorf("'-%s' flag must not be empty", useCaseFlagName)
+	}
+	// repoRoot, err := filepath.Abs(*repoRootParam)
+	// if err != nil {
+	// 	return errors.Wrapf(err, "failed to create absolute path from %s", repoRoot)
+	// }
+
+	glog.Infof("Use case is: %s", *useCase)
+	// glog.Infof("Repo root is: %s", repoRoot)
+
+	gitFs, err := NewGitFS(repository)
+	if err != nil {
+		return errors.Wrapf(err, "failed to initialize git fs")
+	}
+	worktree, err := gitFs.repo.Worktree()
+	if err != nil {
+		return errors.Wrapf(err, "failed to get worktree")
+	}
+
+	switch *useCase {
+	case vpnUseCaseFlagName:
+		// get last used ip and increment
+		lastIP, err := getAndUpdateIP(gitFs, vpnLastIPConfFileRelative)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get valid last ip from file %s", vpnLastIPConfFileRelative)
+		}
+		// insert new vpn client config
+		err = addVPNClient(gitFs, *vpnClientName, *vpnClientPubKey, vpnClientsConfFileRelative, lastIP)
+		if err != nil {
+			return errors.Wrapf(err, "failed to add vpn client")
+		}
+		// commit changes
+		if _, err = worktree.Commit("Added new VPN client config (still testing) [CI SKIP]", &git.CommitOptions{All: true}); err != nil {
+			return errors.Wrapf(err, "failed to commit")
+		}
+	default:
+		err = errors.New(fmt.Sprintf("unsupported use case: %s", *useCase))
+	}
+	if err != nil {
+		return err
+	}
+	if err = gitFs.Push(); err != nil {
+		return errors.Wrapf(err, "failed to push changes")
+	}
+
+	// err = gitRepo()
+	// glog.Infof(err.Error())
+
+	return nil
+}
---
+++ cli/vpn.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.com/pkg/errors"
+)
+
+const (
+	vpnUseCaseFlagName         = "vpn"
+	vpnClientNameFlagName      = "vpn-client-name"
+	vpnClientPubKeyFlagName    = "vpn-pub-key"
+	vpnClientsConfFileRelative = "modules/kubernetes/wireguard/extra/clients.conf"
+	vpnLastIPConfFileRelative  = "modules/kubernetes/wireguard/extra/last_ip.txt"
+)
+
+// addVPNClient inserts new client config
+func addVPNClient(gitFs *GitFS, clientName, publicKey, clientsConfPath, ip string) error {
+	if clientName == "" {
+		return fmt.Errorf("client name must not be empty when creating a new vpn config")
+	}
+	if publicKey == "" {
+		return fmt.Errorf("public key cannot be empty when creating new vpn config")
+	}
+	contents := "[Peer]\n# friendly_name = " + clientName + "\nPublicKey = " + publicKey + "\nAllowedIPs = " + ip + "\n\n"
+	glog.Infof("adding the following config: \n%s", contents)
+	f, err := gitFs.fs.OpenFile(clientsConfPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open client configs file to add new vpn client")
+	}
+	defer f.Close()
+
+	if _, err = f.Write([]byte(contents)); err != nil {
+		return errors.Wrapf(err, "failed to write config to file")
+	}
+
+	glog.Infof("successfully added new vpn client config for %s", clientName)
+	return nil
+}
+
+func incrementIP(origIP, cidr string) (string, error) {
+	ip := net.ParseIP(origIP)
+	_, ipNet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return origIP, err
+	}
+	for i := len(ip) - 1; i >= 0; i-- {
+		ip[i]++
+		if ip[i] != 0 {
+			break
+		}
+	}
+	if !ipNet.Contains(ip) {
+		return origIP, errors.New("overflowed CIDR while incrementing IP")
+	}
+	return ip.String(), nil
+}
+
+// getAndUpdateIP Reads `fileName`, tries to get the ip, increments it, tries to write it back and returns the new address
+func getAndUpdateIP(gitFs *GitFS, fileName string) (string, error) {
+	bytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return "", errors.Wrapf(err, "filed to read file %s", fileName)
+	}
+	errPrefix := "file has incorrect format: "
+	content := strings.TrimSpace(string(bytes))
+	lines := strings.Split(content, "\n")
+	if len(lines) != 1 {
+		return "", fmt.Errorf(errPrefix + fmt.Sprintf("expected 1 line got %d", len(lines)))
+	}
+	lineSplit := strings.Split(lines[0], " ")
+	if len(lineSplit) < 1 {
+		return "", fmt.Errorf("expected non empty line")
+	}
+	ipcidr := strings.Split(lineSplit[len(lineSplit)-1], "/")
+	ipAddr := ipcidr[0]
+	cidr := ipcidr[1]
+	incrementedIP, err := incrementIP(ipAddr, strings.Join(ipcidr, "/"))
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to increment ip for string '%s'", ipcidr)
+	}
+
+	// Write back updated ip
+	fileContents := fmt.Sprintf("# DO NOT MANUALLY EDIT THIS LINE. Last IP: %s", incrementedIP+"/"+cidr)
+	f, err := gitFs.fs.OpenFile(fileName, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to open file %s for writing", fileName)
+	}
+	if _, err = f.Write([]byte(fileContents)); err != nil {
+		return "", errors.Wrapf(err, "failed to write back new ip to file %s contents %s", fileName, fileContents)
+	}
+	glog.Infof("new ip: %s", incrementedIP)
+	return incrementedIP + "/32", nil
+}
