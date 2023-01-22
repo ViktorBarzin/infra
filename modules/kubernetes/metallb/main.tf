65ea868da6b04d628ba4181979cdb17986a6df12
viktorbarzin <vbarzin@gmail.com>
2023-01-22 16:53:34 +0000

    workaround metallb missing resources issue

--- modules/kubernetes/metallb/main.tf
+++ modules/kubernetes/metallb/main.tf
@@ -1,7 +1,13 @@
 # Creates namespace and everythin needed
+# Do not use until https://github.com/colinwilson/terraform-kubernetes-metallb/issues/5 is solved
+# module "metallb" {
+#   source  = "colinwilson/metallb/kubernetes"
+#   version = "0.1.7"
+# }
+
 module "metallb" {
-  source  = "colinwilson/metallb/kubernetes"
-  version = "0.1.7"
+  source  = "ViktorBarzin/metallb/kubernetes"
+  version = "0.1.5"
 }
 
 resource "kubernetes_config_map" "config" {
