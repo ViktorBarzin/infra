57673f09e72346ad26353ceb366c16a06886ee21
Viktor Barzin <vbarzin@gmail.com>
2023-05-10 17:43:16 +0000

    run the check-ip cronjob every 5 minutes [ci skip]

--- modules/kubernetes/infra-maintenance/main.tf
+++ modules/kubernetes/infra-maintenance/main.tf
@@ -1,4 +1,6 @@
 # Module to run some infra-specific things like updating the public ip
+variable git_user {}
+variable git_token {}
 
 
 resource "kubernetes_cron_job_v1" "update-public-ip" {
@@ -7,7 +9,7 @@ resource "kubernetes_cron_job_v1" "update-public-ip" {
     namespace = "default"
   }
   spec {
-    schedule           = "* * * * *"
+    schedule           = "*/5 * * * *"
     concurrency_policy = "Forbid"
     job_template {
       metadata {
@@ -25,6 +27,15 @@ resource "kubernetes_cron_job_v1" "update-public-ip" {
               image = "viktorbarzin/infra"
               command = ["./infra_cli"]
               args    = ["-use-case", "update-public-ip"]
+
+              env {
+                name = "GIT_USER"
+                value = var.git_user
+              }
+              env {
+                name = "GIT_TOKEN"
+                value = var.git_token
+              }
             }
             restart_policy       = "Never"
             # service_account_name = "descheduler-sa"
