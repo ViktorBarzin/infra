b3bbe15c2c93a5f1d20a5bbc1f95a16cf0a57cc5
Viktor Barzin <vbarzin@gmail.com>
2024-02-25 16:28:40 +0000

    add servarr [ci skip]

---
+++ modules/kubernetes/servarr/main.tf
@@ -0,0 +1,21 @@
+variable "tls_secret_name" {}
+
+module "readarr" {
+  source          = "./readarr"
+  tls_secret_name = var.tls_secret_name
+}
+
+module "prowlarr" {
+  source          = "./prowlarr"
+  tls_secret_name = var.tls_secret_name
+}
+
+module "qbittorrent" {
+  source          = "./qbittorrent"
+  tls_secret_name = var.tls_secret_name
+}
+
+module "flaresolverr" {
+  source          = "./flaresolverr"
+  tls_secret_name = var.tls_secret_name
+}
