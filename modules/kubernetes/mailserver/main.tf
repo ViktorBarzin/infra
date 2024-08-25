ddce055b776ba5ffed1d4211e4ed6e011f278881
Viktor Barzin <vbarzin@gmail.com>
2024-08-25 15:36:44 +0000

    add truenas to mail user and move fail2ban logs to stdout as var/log doesnt work for some reason [ci skip]

--- modules/kubernetes/mailserver/main.tf
+++ modules/kubernetes/mailserver/main.tf
@@ -78,6 +78,12 @@ resource "kubernetes_config_map" "mailserver_config" {
     SigningTable  = "*@viktorbarzin.me mail._domainkey.viktorbarzin.me\n"
     TrustedHosts  = "127.0.0.1\nlocalhost\n"
     "sasl_passwd" = var.sasl_passwd
+    fail2ban_conf = <<-EOF
+    [DEFAULT]
+
+    #logtarget = /var/log/fail2ban.log
+    logtarget = SYSOUT
+    EOF
   }
   # Password hashes are different each time and avoid changing secret constantly. 
   # Either 1.Create consistent hashes or 2.Find a way to ignore_changes on per password
@@ -270,6 +276,12 @@ resource "kubernetes_deployment" "mailserver" {
             name       = "config"
             mount_path = "/etc/postfix/sasl/passwd"
             sub_path   = "sasl_passwd"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "config"
+            mount_path = "/etc/fail2ban/fail2ban.local"
+            sub_path   = "fail2ban_conf"
             read_only  = true
           }
           port {
