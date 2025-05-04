2372c94f59bd04111702dcbe1b0a63fe7b1c507d
Viktor Barzin <vbarzin@gmail.com>
2025-05-04 11:24:37 +0000

    add scripts to upgrade ubuntu nodes and upgrade k8s cluster [ci skip]

---
+++ update_node.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# sudo apt update && sudo apt autoremove -y && sudo apt upgrade -y
+sudo do-release-upgrade
+sudo apt update && sudo apt autoremove -y && sudo apt upgrade -y
+
+
+
