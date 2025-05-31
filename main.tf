7b974c387fd587f1978e4f0f74d3afca03c43c16
Viktor Barzin <vbarzin@gmail.com>
2025-05-31 21:50:16 +0000

    use geoapify in dawarich instead of local photon [ci skip]

--- main.tf
+++ main.tf
@@ -98,6 +98,7 @@ variable "cloudflare_non_proxied_names" {}
 variable "cloudflare_tunnel_token" {}
 variable "owntracks_credentials" {}
 variable "dawarich_database_password" {}
+variable "geoapify_api_key" {}
 variable "tandoor_database_password" {}
 variable "n8n_postgresql_password" {}
 
@@ -408,6 +409,7 @@ module "kubernetes_cluster" {
   owntracks_credentials = var.owntracks_credentials
 
   dawarich_database_password = var.dawarich_database_password
+  geoapify_api_key           = var.geoapify_api_key
 
   tandoor_database_password = var.tandoor_database_password
   tandoor_email_password    = var.mailserver_accounts["info@viktorbarzin.me"]
