65a5b92c6c950fe8b9e8981eff85ab76608c9f6e
Viktor Barzin <vbarzin@gmail.com>
2025-07-27 10:31:38 +0000

    add kured helm [ci skip]

---
+++ modules/kubernetes/kured/main.tf
@@ -0,0 +1,31 @@
+variable "tls_secret_name" {}
+variable "notify_url" {}
+
+resource "kubernetes_namespace" "kured" {
+  metadata {
+    name = "kured"
+    labels = {
+      "istio-injection" : "disabled"
+    }
+  }
+}
+
+module "tls_secret" {
+  source          = "../setup_tls_secret"
+  namespace       = "kured"
+  tls_secret_name = var.tls_secret_name
+}
+
+resource "helm_release" "kured" {
+  namespace        = "kured"
+  create_namespace = false
+  name             = "kured"
+
+  repository = "https://kubereboot.github.io/charts"
+  chart      = "kured"
+
+  values = [templatefile("${path.module}/values.yaml", { notify_url : var.notify_url })]
+  atomic = true
+
+  depends_on = [kubernetes_namespace.kured]
+}
