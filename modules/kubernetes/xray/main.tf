8137b76bf946109365925c3696efbb20aaf18687
Viktor Barzin <vbarzin@gmail.com>
2025-08-22 21:41:51 +0000

    add xray along with multiple configs [ci skip]

---
+++ modules/kubernetes/xray/main.tf
@@ -0,0 +1,284 @@
+variable "tls_secret_name" {}
+variable "xray_reality_clients" { type = list(map(string)) }
+variable "xray_reality_private_key" { type = string }
+variable "xray_reality_short_ids" { type = list(string) }
+
+
+module "tls_secret" {
+  source          = "../setup_tls_secret"
+  namespace       = "xray"
+  tls_secret_name = var.tls_secret_name
+}
+
+resource "kubernetes_namespace" "xray" {
+  metadata {
+    name = "xray"
+  }
+}
+
+resource "kubernetes_config_map" "xray_config" {
+  metadata {
+    name      = "xray-config"
+    namespace = "xray"
+
+    labels = {
+      app = "xray"
+    }
+    annotations = {
+      "reloader.stakater.com/match" = "true"
+    }
+  }
+
+  data = {
+    "config.json" = templatefile("${path.module}/xray_config.json.tpl", {
+      clients             = jsonencode(var.xray_reality_clients)
+      reality_private_key = var.xray_reality_private_key
+      reality_short_ids   = jsonencode(var.xray_reality_short_ids)
+    })
+  }
+}
+
+resource "kubernetes_deployment" "xray" {
+  metadata {
+    name      = "xray"
+    namespace = "xray"
+    labels = {
+      app = "xray"
+    }
+    annotations = {
+      "reloader.stakater.com/search" = "true"
+    }
+  }
+  spec {
+    replicas = 1
+    strategy {
+      rolling_update {
+        max_surge       = "2"
+        max_unavailable = "0"
+      }
+    }
+    selector {
+      match_labels = {
+        app = "xray"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "xray"
+        }
+      }
+      spec {
+        container {
+          image             = "teddysun/xray"
+          name              = "xray"
+          image_pull_policy = "IfNotPresent"
+          port {
+            container_port = 6443 // vless
+            protocol       = "TCP"
+          }
+          port {
+            container_port = 7443 // reality
+            protocol       = "TCP"
+          }
+          port {
+            container_port = 8443 // websocket
+            protocol       = "TCP"
+          }
+          port {
+            container_port = 9443 // gRPC
+            protocol       = "TCP"
+          }
+          volume_mount {
+            name       = "tls"
+            mount_path = "/etc/xray/tls.crt"
+            sub_path   = "tls.crt"
+          }
+          volume_mount {
+            name       = "tls"
+            mount_path = "/etc/xray/tls.key"
+            sub_path   = "tls.key"
+          }
+          volume_mount {
+            name       = "config"
+            mount_path = "/etc/xray/config.json"
+            sub_path   = "config.json"
+          }
+        }
+
+        volume {
+          name = "tls"
+          secret {
+            secret_name = var.tls_secret_name
+          }
+        }
+        volume {
+          name = "config"
+          config_map {
+            name = "xray-config"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "xray" {
+  metadata {
+    name      = "xray"
+    namespace = "xray"
+    labels = {
+      "app" = "xray"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "xray"
+    }
+    port {
+      name     = "vless"
+      port     = 6443
+      protocol = "TCP"
+    }
+    port {
+      name     = "websocket"
+      port     = 8443
+      protocol = "TCP"
+    }
+    port {
+      name     = "grpc"
+      port     = 9443
+      protocol = "TCP"
+    }
+  }
+}
+
+resource "kubernetes_service" "xray-reality" {
+  metadata {
+    name      = "xray-reality"
+    namespace = "xray"
+    labels = {
+      "app" = "xray"
+    }
+  }
+
+  spec {
+    type = "LoadBalancer"
+    selector = {
+      app = "xray"
+    }
+    port {
+      name     = "reality"
+      port     = 7443
+      protocol = "TCP"
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "ingress" {
+  metadata {
+    namespace = "xray"
+    name      = "xray"
+    annotations = {
+      "kubernetes.io/ingress.class"                  = "nginx"
+      "nginx.ingress.kubernetes.io/backend-protocol" = "HTTP"
+      "nginx.org/websocket-services" : "xray"
+      "nginx.ingress.kubernetes.io/enable-access-log" = "false"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = ["xray-ws.viktorbarzin.me"]
+      secret_name = var.tls_secret_name
+    }
+    rule {
+      host = "xray-ws.viktorbarzin.me"
+      http {
+        path {
+          backend {
+            service {
+              name = "xray"
+              port {
+                number = 8443
+
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "ingress-grpc" {
+  metadata {
+    namespace = "xray"
+    name      = "xray-grpc"
+    annotations = {
+      "kubernetes.io/ingress.class"                    = "nginx"
+      "nginx.ingress.kubernetes.io/enable-access-log"  = "false"
+      "nginx.ingress.kubernetes.io/backend-protocol"   = "GRPC"
+      "nginx.ingress.kubernetes.io/proxy-read-timeout" = "3600"
+      "nginx.ingress.kubernetes.io/proxy-send-timeout" = "3600"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = ["xray-grpc.viktorbarzin.me"]
+      secret_name = var.tls_secret_name
+    }
+    rule {
+      host = "xray-grpc.viktorbarzin.me"
+      http {
+        path {
+          path      = "/grpc-vpn"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "xray"
+              port {
+                number = 9443
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "ingress-vless" {
+  metadata {
+    namespace = "xray"
+    name      = "xray-vless"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = ["xray-vless.viktorbarzin.me"]
+      secret_name = var.tls_secret_name
+    }
+    rule {
+      host = "xray-vless.viktorbarzin.me"
+      http {
+        path {
+          backend {
+            service {
+              name = "xray"
+              port {
+                number = 6443
+
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
