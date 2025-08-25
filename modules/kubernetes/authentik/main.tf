4b78a512928f41f7091f0b4b2fbfd8c069261ad6
Viktor Barzin <vbarzin@gmail.com>
2025-08-25 13:24:12 +0000

    upgrade authentik to 2025.8.1 [ci skip]

--- modules/kubernetes/authentik/main.tf
+++ modules/kubernetes/authentik/main.tf
@@ -22,7 +22,7 @@ resource "helm_release" "authentik" {
 
   repository = "https://charts.goauthentik.io/"
   chart      = "authentik"
-  version    = "2025.6.3"
+  version    = "2025.8.1"
   atomic     = true
   timeout    = 6000
 
