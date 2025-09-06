7eacc9d6c8aec46f964d1db79516300ef5e940db
Viktor Barzin <vbarzin@gmail.com>
2025-09-06 20:10:17 +0000

    add qbittorrent from servarr setup [ci skip]

--- modules/kubernetes/servarr/main.tf
+++ modules/kubernetes/servarr/main.tf
@@ -5,17 +5,17 @@ variable "tls_secret_name" {}
 #   tls_secret_name = var.tls_secret_name
 # }
 
-module "prowlarr" {
-  source          = "./prowlarr"
-  tls_secret_name = var.tls_secret_name
-}
+# module "prowlarr" {
+#   source          = "./prowlarr"
+#   tls_secret_name = var.tls_secret_name
+# }
 
 module "qbittorrent" {
   source          = "./qbittorrent"
   tls_secret_name = var.tls_secret_name
 }
 
-module "flaresolverr" {
-  source          = "./flaresolverr"
-  tls_secret_name = var.tls_secret_name
-}
+# module "flaresolverr" {
+#   source          = "./flaresolverr"
+#   tls_secret_name = var.tls_secret_name
+# }
--- modules/kubernetes/servarr/qbittorrent/main.tf
+++ modules/kubernetes/servarr/qbittorrent/main.tf
@@ -99,7 +99,7 @@ resource "kubernetes_service" "qbittorrent" {
     }
     port {
       name        = "http"
-      port        = 8080
+      port        = 80
       target_port = 8080
     }
   }
@@ -138,43 +138,15 @@ resource "kubernetes_service" "qbittorrent-torrenting" {
   }
 }
 
-resource "kubernetes_ingress_v1" "qbittorrent" {
-  metadata {
-    name      = "qbittorrent"
-    namespace = "qbittorrent"
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-      # "nginx.ingress.kubernetes.io/auth-url" : "https://oauth2.viktorbarzin.me/oauth2/auth"
-      # "nginx.ingress.kubernetes.io/auth-signin" : "https://oauth2.viktorbarzin.me/oauth2/start?rd=/redirect/$http_host$escaped_request_uri"
-      "nginx.ingress.kubernetes.io/auth-url" : "http://ak-outpost-authentik-embedded-outpost.authentik.svc.cluster.local:9000/outpost.goauthentik.io/auth/nginx"
-      "nginx.ingress.kubernetes.io/auth-signin" : "https://authentik.viktorbarzin.me/outpost.goauthentik.io/start?rd=$scheme%3A%2F%2F$host$escaped_request_uri"
 
-      "nginx.ingress.kubernetes.io/auth-response-headers" : "Set-Cookie,X-authentik-username,X-authentik-groups,X-authentik-email,X-authentik-name,X-authentik-uid"
-      "nginx.ingress.kubernetes.io/auth-snippet" : "proxy_set_header X-Forwarded-Host $http_host;"
-      "nginx.ingress.kubernetes.io/proxy-body-size" : "100000m" // allow uploading .torrent files
-    }
+module "ingress" {
+  source          = "../../ingress_factory"
+  namespace       = "qbittorrent"
+  name            = "qbittorrent"
+  tls_secret_name = var.tls_secret_name
+  protected       = true
+  extra_annotations = {
+    "nginx.ingress.kubernetes.io/proxy-body-size" : "1G" // allow uploading .torrent files
   }
 
-  spec {
-    tls {
-      hosts       = ["qbittorrent.viktorbarzin.me"]
-      secret_name = var.tls_secret_name
-    }
-    rule {
-      host = "qbittorrent.viktorbarzin.me"
-      http {
-        path {
-          path = "/"
-          backend {
-            service {
-              name = "qbittorrent"
-              port {
-                number = 8080
-              }
-            }
-          }
-        }
-      }
-    }
-  }
 }
