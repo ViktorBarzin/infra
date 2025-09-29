9be44437e649f6af30f63f90e9443fab59e21a09
Viktor Barzin <vbarzin@gmail.com>
2025-09-29 20:29:24 +0000

    add immich frame [ci skip]

---
+++ modules/kubernetes/immich/frame.tf
@@ -0,0 +1,114 @@
+variable "frame_api_key" {
+  type = string
+}
+
+resource "kubernetes_config_map" "mailserver_config" {
+  metadata {
+    name      = "config"
+    namespace = "immich"
+
+    labels = {
+      app = "frame-config"
+    }
+    annotations = {
+      "reloader.stakater.com/match" = "true"
+    }
+  }
+
+  data = {
+    # Actual mail settings
+    "Settings.yml" = <<-EOF
+    General:
+        Layout: single
+        Interval: 10
+        ImageZoom: false
+        ShowAlbumName: false
+    Accounts:
+        - ImmichServerUrl: http://immich.viktorbarzin.me
+          ApiKey: ${var.frame_api_key}
+          Albums: 
+            - 1aa98849-bbd5-452b-aac0-310b210a8597 # china
+    EOF
+  }
+}
+
+
+resource "kubernetes_deployment" "immich-frame" {
+  metadata {
+    name      = "immich-frame"
+    namespace = "immich"
+    annotations = {
+      "reloader.stakater.com/search" = "true"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "immich-frame"
+      }
+    }
+    strategy {
+      type = "Recreate"
+    }
+    template {
+      metadata {
+        labels = {
+          app = "immich-frame"
+        }
+      }
+      spec {
+        container {
+          image = "ghcr.io/immichframe/immichframe:latest"
+          name  = "immich-frame"
+          port {
+            container_port = 8080
+            protocol       = "TCP"
+            name           = "http"
+          }
+          volume_mount {
+            name       = "config"
+            mount_path = "/app/Config"
+            read_only  = true
+          }
+        }
+        volume {
+          name = "config"
+          config_map {
+            name = "config"
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service" "immich-frame" {
+  metadata {
+    name      = "immich-frame"
+    namespace = "immich"
+    labels = {
+      "app" = "immich-frame"
+    }
+  }
+
+  spec {
+    selector = {
+      app = "immich-frame"
+    }
+    port {
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
+
+module "ingress" {
+  source          = "../ingress_factory"
+  namespace       = "immich"
+  name            = "highlights-immich"
+  tls_secret_name = var.tls_secret_name
+  service_name    = "immich-frame"
+}
