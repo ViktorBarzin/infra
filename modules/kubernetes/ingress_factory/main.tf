13b7c880e50d9f888fc8deacff810557e9f480c0
Viktor Barzin <vbarzin@gmail.com>
2025-10-13 20:12:37 +0000

    add crowdsec policies for 403 and 429; use nginx to rate limit brute force attacks and then ban them [ci skip]

--- modules/kubernetes/ingress_factory/main.tf
+++ modules/kubernetes/ingress_factory/main.tf
@@ -88,9 +88,9 @@ resource "kubernetes_ingress_v1" "proxied-ingress" {
       "nginx.ingress.kubernetes.io/proxy-buffering" : "on"
 
       # DDOS protection
-      "nginx.ingress.kubernetes.io/limit-connections" : 500
+      "nginx.ingress.kubernetes.io/limit-connections" : 100
       "nginx.ingress.kubernetes.io/limit-rps" : 10
-      "nginx.ingress.kubernetes.io/limit-rpm" : 60
+      "nginx.ingress.kubernetes.io/limit-rpm" : 30
       "nginx.ingress.kubernetes.io/limit-burst-multiplier" : 10
       "nginx.ingress.kubernetes.io/limit-rate-after" : 10
       "nginx.ingress.kubernetes.io/configuration-snippet" = <<-EOF
