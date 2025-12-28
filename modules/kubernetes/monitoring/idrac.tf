95a670836138dafe092fd2dc20864e21e293547d
Viktor Barzin <vbarzin@gmail.com>
2025-12-28 20:07:00 +0000

    move out all monitoring resources to separate tf files [ci skip]

---
+++ modules/kubernetes/monitoring/idrac.tf
@@ -0,0 +1,113 @@
+
+resource "kubernetes_config_map" "redfish-config" {
+  metadata {
+    name      = "redfish-exporter-config"
+    namespace = "monitoring"
+
+    annotations = {
+      "reloader.stakater.com/match" = "true"
+    }
+  }
+  data = {
+    "config.yml" = <<-EOF
+      address: 0.0.0.0
+      port: 9610
+      hosts:
+        ${var.idrac_host}:
+          username: ${var.idrac_username}
+          password: ${var.idrac_password}
+        default:
+          username: root
+          password: calvin
+      metrics:
+        all: true
+    EOF
+  }
+}
+
+resource "kubernetes_deployment" "idrac-redfish" {
+  metadata {
+    name      = "idrac-redfish-exporter"
+    namespace = "monitoring"
+    labels = {
+      app = "idrac-redfish-exporter"
+    }
+    annotations = {
+      "reloader.stakater.com/search" = "true"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "idrac-redfish-exporter"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "idrac-redfish-exporter"
+        }
+      }
+      spec {
+        container {
+          # https://github.com/mrlhansen/idrac_exporter?tab=readme-ov-file
+          image = "ghcr.io/mrlhansen/idrac_exporter:latest"
+          name  = "redfish-exporter"
+          port {
+            container_port = 9610
+          }
+
+          volume_mount {
+            name       = "redfish-exporter-config"
+            mount_path = "/etc/prometheus/idrac.yml"
+            sub_path   = "config.yml"
+          }
+        }
+        volume {
+          name = "redfish-exporter-config"
+          config_map {
+            name = "redfish-exporter-config"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "idrac-redfish-exporter" {
+  metadata {
+    name      = "idrac-redfish-exporter"
+    namespace = "monitoring"
+    labels = {
+      "app" = "idrac-redfish-exporter"
+    }
+    annotations = {
+      "prometheus.io/scrape" = "true"
+      "prometheus.io/path"   = "/metrics"
+      "prometheus.io/port"   = "9090"
+    }
+  }
+
+  spec {
+    selector = {
+      "app" = "idrac-redfish-exporter"
+    }
+    port {
+      name        = "http"
+      port        = "9090"
+      target_port = "9610"
+    }
+  }
+}
+
+module "idrac-redfish-exporter-ingress" {
+  source                  = "../ingress_factory"
+  namespace               = "monitoring"
+  name                    = "idrac-redfish-exporter"
+  root_domain             = "viktorbarzin.lan"
+  tls_secret_name         = var.tls_secret_name
+  allow_local_access_only = true
+  ssl_redirect            = false
+  port                    = 9090
+}
---
+++ modules/kubernetes/monitoring/snmp_exporter.tf
@@ -0,0 +1,112 @@
+
+/**
+1. clone snmp exporter
+2. update generator.yaml to include only interesting modules
+3. make generate
+4. cp snmp.yml to whereever is used
+5. scrape service with curl 'http://snmp-exporter.monitoring.svc.cluster.local:9116/snmp?auth=public_v2&module=huawei&target=192.168.1.5%3A161'
+
+generate reference - https://github.com/prometheus/snmp_exporter/tree/main/generator
+https://sbcode.net/prometheus/snmp-generate-huawei/
+*/
+resource "kubernetes_config_map" "snmp-exporter-yaml" {
+  metadata {
+    name      = "snmp-exporter-yaml"
+    namespace = "monitoring"
+
+    annotations = {
+      "reloader.stakater.com/match" = "true"
+    }
+  }
+  data = {
+    "snmp.yml" = file("${path.module}/ups_snmp_values.yaml")
+
+  }
+}
+
+resource "kubernetes_deployment" "snmp-exporter" {
+  metadata {
+    name      = "snmp-exporter"
+    namespace = "monitoring"
+    labels = {
+      app = "snmp-exporter"
+    }
+    annotations = {
+      "reloader.stakater.com/search" = "true"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "snmp-exporter"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "snmp-exporter"
+        }
+      }
+      spec {
+        container {
+          image = "prom/snmp-exporter"
+          name  = "snmp-exporter"
+          # command = ["/usr/local/bin/redfish_exporter", "--config.file", "/app/config.yml"]
+          port {
+            container_port = 9116
+          }
+
+          volume_mount {
+            name       = "config-volume"
+            mount_path = "/etc/snmp_exporter/"
+          }
+        }
+        volume {
+          name = "config-volume"
+
+          config_map {
+            name = "snmp-exporter-yaml"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "snmp-exporter" {
+  metadata {
+    name      = "snmp-exporter"
+    namespace = "monitoring"
+    labels = {
+      "app" = "snmp-exporter"
+    }
+    annotations = {
+      "prometheus.io/scrape" = "true"
+      "prometheus.io/path"   = "/snmp?auth=Public0&target=tcp%3A%2F%2F192.%3A161"
+      "prometheus.io/port"   = "9116"
+    }
+  }
+
+  spec {
+    selector = {
+      "app" = "snmp-exporter"
+    }
+    port {
+      name        = "http"
+      port        = "9116"
+      target_port = "9116"
+    }
+  }
+}
+
+module "snmp-exporter-ingress" {
+  source                  = "../ingress_factory"
+  namespace               = "monitoring"
+  name                    = "snmp-exporter"
+  root_domain             = "viktorbarzin.lan"
+  tls_secret_name         = var.tls_secret_name
+  allow_local_access_only = true
+  ssl_redirect            = false
+  port                    = 9116
+}
