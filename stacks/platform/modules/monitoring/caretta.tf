e8997ec430c46488819d375e3394a916c0ade3a1
Viktor Barzin <viktorbarzin@meta.com>
2026-02-28 18:30:20 +0000

    [ci skip] add caretta, goflow2, and prometheus scrape targets to monitoring module

--- stacks/platform/modules/monitoring/caretta.tf
+++ stacks/platform/modules/monitoring/caretta.tf
@@ -18,6 +18,27 @@ resource "helm_release" "caretta" {
   }
 }
 
+resource "kubernetes_service" "caretta_metrics" {
+  metadata {
+    name      = "caretta-metrics"
+    namespace = kubernetes_namespace.monitoring.metadata[0].name
+    labels = {
+      app = "caretta"
+    }
+  }
+  spec {
+    selector = {
+      app = "caretta"
+    }
+    port {
+      name        = "metrics"
+      port        = 7117
+      target_port = 7117
+      protocol    = "TCP"
+    }
+  }
+}
+
 resource "kubernetes_config_map" "caretta_grafana_dashboard" {
   metadata {
     name      = "caretta-grafana-dashboard"
