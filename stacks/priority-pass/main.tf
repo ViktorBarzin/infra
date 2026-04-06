0162b4f130e9a456eff6ccb6f9fd66fe1615875c
Viktor Barzin <viktorbarzin@meta.com>
2026-04-06 13:09:45 +0300

    priority-pass: update backend to v6 (remove edge artifact scratches)

--- stacks/priority-pass/main.tf
+++ stacks/priority-pass/main.tf
@@ -71,7 +71,7 @@ resource "kubernetes_deployment" "priority-pass" {
         }
         container {
           name  = "backend"
-          image = "registry.viktorbarzin.me/priority-pass-backend:v5"
+          image = "registry.viktorbarzin.me/priority-pass-backend:v6"
           port {
             container_port = 8000
           }
